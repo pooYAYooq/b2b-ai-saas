f6668cc3a094642c7f78e41d35e29b99d6d0bbcc
pooYAYooq <pooyayooq@gmail.com>
2025-12-01 20:11:05 +1100

    feat: replace placeholder with header and hero section
    Updated the Home component to include HeroHeader and HeroSection, removing the placeholder text and theme toggle for a more cohesive layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 import { ThemeToggle } from "@/components/ui/them-toggle";
 
 export default function Home() {
   return (
     <div>
-      <h1>P2: B2B - AI - SaaS</h1>
-      <ThemeToggle />
+      <HeroHeader />
+      <HeroSection />
     </div>
   );
 }
